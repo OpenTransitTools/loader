520468fe1f8da19525cdff2b0d4a7c9714b4caab
Frank Purcell <fxpurcell@gmail.com>
2015-01-22 20:11:37 -0800

    random

--- ott/deploy/otp/random_trip.py
+++ ott/deploy/otp/random_trip.py
@@ -7,30 +7,48 @@ from ott.utils.parse import csv_reader
 from tm_ws_runner import WsTest
 from test_runner import Test
 
+
+def_header="Description/notes,From,To,Mode,Time,Service,Optimize,Max dist,Arrive by,Depart by,Expected output,Expected number of legs,Expected trip duration,Expected trip distance,Passes?"
+
 class RandomTrip():
     """ read in a .csv file full of points, and then output a set of test cases
     """
-    def __init__(self):
+    def __init__(self, num=500):
         self.name_list = []
-        self.url_list = []
+        self.from_to_list = []
 
         geotests = csv_reader.Csv.get_relative_dirname(__file__, "../../geocode/tests/geocodes.csv")
         csv_file = csv_reader.Csv('geocodes.csv', geotests)
         self.test_data = csv_file.open()
+
         for t in self.test_data:
             self.name_list.append(t['name'])
 
-    def make_tests(self, planner_url, params="fromPlace={0}&toPlace={1}", num=500):
         for i in xrange(num):
             s = random.sample(self.name_list, 2)
-            p1 = s[0].replace(' ', '%20').replace('&', '%26').replace('#', '%23')
-            p2 = s[1].replace(' ', '%20').replace('&', '%26').replace('#', '%23')
-            p = params.format(p1, p2)
-            u = planner_url + "?" + p
-            self.url_list.append(u)
+            self.from_to_list.append(s)
 
-    def call_urls(self):
-        for u in self.url_list:
+    @classmethod
+    def escape(cls, s):
+        return s.replace(' ', '%20').replace('&', '%26').replace('#', '%23')
+
+    @classmethod
+    def make_test_urls(cls, planner_url, from_to_list, params="fromPlace={0}&toPlace={1}"):
+        url_list = []
+        for p in from_to_list:
+            p = params.format(cls.escape(p[0]), cls.escape(p[1]))
+            u = planner_url + "?" + p
+            url_list.append(u)
+        return url_list
+
+    @classmethod
+    def make_suite_csv(cls, from_to_list, file_name=None, suite_header=def_header, suite_format="test {0},{1},{2},,4:40 PM,,,,,,leg,,,,"):
+        print suite_header
+        for i, p in enumerate(from_to_list):
+            print suite_format.format(i+1, *p)
+
+    def call_urls(self, url_list):
+        for u in url_list:
             #import pdb; pdb.set_trace()
             itinerary = Test.static_call_otp(u)
             print u
@@ -52,18 +70,18 @@ def ws_trips():
     h = WsTest.make_hostname()
     p,m = WsTest.make_urls(h)
     rt = RandomTrip()
-    rt.make_tests(p)
-    rt.call_urls()
+    rt.make_suite_csv(rt.from_to_list)
 
 def otp_trips():
     h = Test.make_hostname()
     p,m = Test.make_urls(h)
-    rt = RandomTrip(p)
+    rt = RandomTrip()
     rt.make_tests(p)
     rt.call_urls()
 
 def main(argv=sys.argv):
-    otp_trips()
+    #otp_trips()
+    ws_trips()
 
 if __name__ == '__main__':
     main()
