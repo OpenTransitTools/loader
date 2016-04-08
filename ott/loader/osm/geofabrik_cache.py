6b22e0fb95e9d6b8bb3f63b9450dea99e6e88a32
Frank Purcell <fxpurcell@gmail.com>
2016-04-08 14:04:45 -0700

    get osm / geofab working as cmd line app

--- ott/loader/osm/geofabrik_cache.py
+++ ott/loader/osm/geofabrik_cache.py
@@ -10,11 +10,11 @@ class GeoFabrikCache(OsmCache):
          1. it will look to see if a gtfs.zip file is in the cache, and download it and put it in the cache if not
          2. once cached, it will check to see that the file in the cache is the most up to date data...
     """
-    pbf_url = ""
-    meta_url = ""
+    pbf_url = "http://download.geofabrik.de/north-america/us-west-latest.osm.pbf"
+    meta_url = "http://download.geofabrik.de/north-america/us-west.html"
 
-    def __init__(self, force_download=False):
-        super().__init__(args)
+    def __init__(self, name, force_download=False):
+        super(GeoFabrikCache, self).__init__(name, pbf_url=self.meta_url, meta_url=self.meta_url, force_download=force_download)
 
     def download_pbf(self):
         file_utils.bkup(self.pbf_path)
@@ -22,7 +22,8 @@ class GeoFabrikCache(OsmCache):
 
 
 def main():
-    pass
+    import pdb; pdb.set_trace()
+    g = GeoFabrikCache(name="or-wa")
 
 if __name__ == '__main__':
     main()
