8b93904a6ae1a899d8efa93eb6f1700009a8ef56
Frank Purcell <fxpurcell@gmail.com>
2016-04-24 09:13:40 -0700

    config reader part of gtfsdb loader now

--- ott/loader/gtfsdb/load.py
+++ ott/loader/gtfsdb/load.py
@@ -1,7 +1,5 @@
 import sys
-import json
 
-from ott.utils.config_util import ConfigUtil
 from ott.utils.cache_base import CacheBase
 from ott.loader.gtfs.cache import Cache
 
@@ -11,11 +9,10 @@ class Load(CacheBase):
     gtfs_zip_files  = None
 
     def __init__(self, config=None, force_reload=False):
-        super(CacheBase, self).__init__()
+        super(Load, self).__init__(section='gtfs')
         reload = force_reload
-        self.config.section = 'gtfs'
 
-        feeds = config.get_json('feeds')
+        feeds = self.config.get_json('feeds')
         if Cache.check_gtfs_files_against_cache(feeds, self.cache_dir):
             reload = True
         if reload:
@@ -23,7 +20,7 @@ class Load(CacheBase):
             print "TODO ... implement load of gtfsdb"
 
 def main(argv=sys.argv):
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
     Load()
 
 if __name__ == '__main__':
