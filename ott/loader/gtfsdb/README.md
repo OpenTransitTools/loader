c55e579a7704de32c1bf7e04f511576d110d2968
Frank Purcell <fxpurcell@gmail.com>
2016-04-28 17:02:06 -0700

    more db loader work

--- ott/loader/gtfsdb/README.md
+++ ott/loader/gtfsdb/README.md
@@ -5,3 +5,15 @@ purpose: check to see if the cached GTFS .zip files have been updated, and if so
          The [./config/app.ini](../../../config/app.ini) file controls the list of gtfs feeds cached, and the db details.
 
 run: bin/gtfsdb_load (optional -ini <name>.ini | force_update)
+
+PostGIS database:
+     http://postgis.net/docs/postgis_installation.html#create_new_db_extensions
+     createdb ott
+     createuser ott
+     psql  -d ott -c "CREATE EXTENSION postgis;"
+
+     MAC:
+       /Applications/Postgres.app/Contents/Versions/9.4/bin/createdb ott
+       /Applications/Postgres.app/Contents/Versions/9.4/bin/createuser ott
+       /Applications/Postgres.app/Contents/Versions/9.4/bin/psql  -d ott -c "CREATE EXTENSION postgis;"
+
--- ott/loader/gtfsdb/load.py
+++ ott/loader/gtfsdb/load.py
@@ -5,19 +5,22 @@ from ott.loader.gtfs.gtfs_cache import GtfsCache
 class Load(CacheBase):
     """ load GTFS data into a gtfsdb
     """
-
-
+    feeds = []
+    url = None
+    is_geospatial = False
 
     def __init__(self, force_update=False):
         super(Load, self).__init__(section='gtfs')
 
         # step 1: config
-        feeds = self.config.get_json('feeds')
+        self.feeds  = self.config.get_json('feeds')
+        self.url    = self.config.get_json('url', section='db')
+        self.is_geospatial = self.config.get_json('is_geospatial',  section='db')
 
         # step 2: check the cache whether we should update or not
         reload = force_update
         if not force_update:
-            if GtfsCache.check_gtfs_files_against_cache(feeds, self.cache_dir):
+            if GtfsCache.check_gtfs_files_against_cache(self.feeds, self.cache_dir):
                 reload = True
 
         # step 3: reload database
@@ -28,7 +31,9 @@ class Load(CacheBase):
     def load_db(self):
         ''' insert
         '''
-        
+        for f in self.feeds:
+            print f
+
 
 def main():
     #import pdb; pdb.set_trace()
