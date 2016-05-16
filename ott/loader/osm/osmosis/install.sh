d182687ea4a40129b7bf8693b77a46d7c2e61c3a
Frank Purcell <fxpurcell@gmail.com>
2016-05-16 12:44:22 -0700

    add exe perms to .bat file

--- ott/loader/osm/osmosis/install.sh
+++ ott/loader/osm/osmosis/install.sh
@@ -2,4 +2,5 @@ rm osmosis-latest.tgz
 wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz
 tar xvfz osmosis-latest.tgz
 chmod a+x bin/osmosis
+chmod a+x bin/osmosis.bat
 bin/osmosis
