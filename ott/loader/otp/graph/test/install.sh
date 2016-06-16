551336c6e8b5ed2d7486bd144996a0a7d74ba134
Frank Purcell <fxpurcell@gmail.com>
2016-06-15 18:21:15 -0700

    make GRAPH name the name of the otp cache file (by default)

--- ott/loader/otp/graph/test/install.sh
+++ ott/loader/otp/graph/test/install.sh
@@ -1,5 +1,8 @@
+# name the GRAPH (based on directory name where this script is, ala 'prod'
+GRAPH_DIR=$(dirname "$0")
+GRAPH_NAME=${GRAPH_DIR##*/}
+
 # src the lib file
-GRAPH_NAME=prod
 . ott/loader/otp/graph/config/install-base.sh
 
 ## build / copy .jar into place
