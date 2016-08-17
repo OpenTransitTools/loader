c4e1228ff6a5c0b4597fbd0754d1c8615e5c0585
Frank Purcell <fxpurcell@gmail.com>
2016-08-17 16:39:15 -0700

    load SOLR working

--- ott/loader/solr/load.py
+++ ott/loader/solr/load.py
@@ -3,6 +3,7 @@ import logging
 log = logging.getLogger(__file__)
 
 from ott.utils import file_utils
+from ott.utils import web_utils
 from ott.loader.solr.solr_cache import SolrCache
 
 
@@ -42,41 +43,75 @@ class Load(object):
         for f in files:
             self._process_file(f)
 
-    def commit(self):
-        ''' send solr_commit.xml to SOLR to commit any index changes
+    @classmethod
+    def commit(cls, url):
+        ''' send commit (<commit/>) message to SOLR, which persists index changes to SOLR
         '''
+        status = web_utils.post_data(url, "<commit/>")
+        if status != 200:
+            log.info("HTTP STATUS: {} when posting a 'commit' command to SOLR instance {}".format(status, url))
+        return status
 
-    def optimize(self):
-        ''' send solr_optimize.xml (<optimize/>) to SOLR to commit any index changes
+    @classmethod
+    def optimize(cls, url):
+        ''' send optimize (<optimize/>) message to SOLR, which fixes up the SOLR instance
         '''
+        status = web_utils.post_data(url, "<optimize/>")
+        if status != 200:
+            log.info("HTTP STATUS: {} when posting a 'optimize' command to SOLR instance {}".format(status, url))
+        return status
+
+    @classmethod
+    def post_file(cls, url, solr_xml_file_path):
+        ''' post a file to a SOLR instance
+        '''
+        status = web_utils.post_file(url, solr_xml_file_path)
+        if status != 200:
+            log.info("HTTP STATUS: {} when posting file {} command to SOLR instance {}".format(status, solr_xml_file_path, url))
+        return status
+
+    @classmethod
+    def update_index(cls, url, solr_xml_file_path):
+        ''' update a SOLR index via a single instance
+            NOTE: if multiple instances of SOLR point to the same instance, only instance needs to call this routine
+        '''
+        status = cls.post_file(url, solr_xml_file_path)
+        cls.commit(url)
+        cls.optimize(url)
+        return status == 200
 
     def _process_file(self, file_name):
         '''
         '''
+        is_success = False
 
         # step 1: grab file path
-        file_path = os.path.join(self.cache.cache_dir, file_name)
-        print file_path
+        solr_xml_file_path = os.path.join(self.cache.cache_dir, file_name)
+        log.debug(solr_xml_file_path)
 
         # step 2: grab SOLR properties for url (and optionally the web ports where SOLR instance(es) run
         url  = self.config.get('url')
-        port = None
+        ports = None
         if ":{}" in url or ":{0}" in url:
             ports = self.config.get_list('ports', def_val='80')
 
         # step 3: update SOLR
         if ports:
             # step 3a: update one instance of SOLR (assumes they use a shared index)
-            print url.format(ports[0])
+            u = url.format(ports[0])
+            is_success = self.update_index(u, solr_xml_file_path)
 
             # step 3b: refresh all instances of SOLR
-            for p in ports:
-                print url.format(p)
+            for p in ports[:-1]:
+                u = url.format(p)
+                self.commit(u)
         else:
             # step 3c: update and refresh the single instance of SOLR
-            print url
-            print url
+            is_success = self.update_index(url, solr_xml_file_path)
 
         # step 4: mv file to processed folder
-        to_path = os.path.join(self.post_process_dir, file_name)
-        #file_utils.mv(file_path, to_path)
+        if is_success:
+            to_path = os.path.join(self.post_process_dir, file_name)
+            file_utils.mv(file_path, to_path)
+
+        return is_success
