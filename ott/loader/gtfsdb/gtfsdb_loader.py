a0a0b493637c38d730695aacf2a85fd7b532ae7d
Frank Purcell <fxpurcell@gmail.com>
2016-08-22 16:38:33 -0700

    have db create itself if it doesn't exist

--- ott/loader/gtfsdb/gtfsdb_loader.py
+++ ott/loader/gtfsdb/gtfsdb_loader.py
@@ -3,6 +3,7 @@ import logging
 log = logging.getLogger(__file__)
 
 from ott.utils import object_utils
+from ott.utils import db_utils
 from ott.utils.cache_base import CacheBase
 from ott.loader.gtfs.gtfs_cache import GtfsCache
 from gtfsdb.api import database_load
@@ -18,8 +19,9 @@ class GtfsdbLoader(CacheBase):
     def __init__(self):
         super(GtfsdbLoader, self).__init__(section='gtfs')
 
-        self.feeds  = self.config.get_json('feeds', section='gtfs')
-        self.db_url = self.config.get('url', section='db')
+        self.feeds   = self.config.get_json('feeds', section='gtfs')
+        self.db_url  = self.config.get('url', section='db', def_val='postgresql+psycopg2://127.0.0.1:5432/ott')
+        self.db_user = self.config.get('user', section='db')
         self.is_geospatial = self.config.get_bool('is_geospatial', section='db')
 
     def check_db(self, force_update=False):
@@ -30,6 +32,9 @@ class GtfsdbLoader(CacheBase):
             reload = True
 
         if reload:
+            # step 1: check the database
+            db_utils.check_create_db(self.db_url, self.db_user, self.is_geospatial)
+
             self.load_feeds()
 
     def load_feeds(self):
