1a002691d98e3b2bb818d2f4191b5a70964b012b
Frank Purcell <fxpurcell@gmail.com>
2016-09-08 15:51:10 -0700

    fix up the id from SOBI to remove the hub_

--- ott/loader/sum/gbfs/gbfs_cache.py
+++ ott/loader/sum/gbfs/gbfs_cache.py
@@ -29,12 +29,23 @@ class GbfsCache(CacheBase):
             ret_val = self.to_solr(stations)
         return ret_val
 
+    @classmethod
+    def get_clean_id(cls, station, def_val=-111):
+        ''' remove hub_ prefix crap from SOBI ids, etc...
+            note that: https://app.socialbicycles.com/map?hub_id=1581 works, but hub_1581 not so much...
+        '''
+        id = station.get('station_id', def_val)
+        if isinstance(id, (str, unicode)):
+            id = id.replace('hub_', '')
+        return str(id)
+
     def to_solr(self, stations):
         solr_add = SolrAdd(type=self.type, type_name=self.name)
         for i, s in enumerate(stations.active_stations()):
             #status = s.get('status')
             station = s.get('station')
-            solr_add.new_doc(id=str(station.get('station_id', i)), name=station.get('name'))
+            id = self.get_clean_id(station, i)
+            solr_add.new_doc(id=id, name=station.get('name'))
             solr_add.add_field('address', station.get('address'))
             solr_add.add_lon_lat(station.get('lon'), station.get('lat'))
 
