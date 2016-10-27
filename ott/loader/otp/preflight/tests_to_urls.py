21ee6e3cc6442b5768bd5c8d0dc753d15b1b97ca
Frank Purcell <fxpurcell@gmail.com>
2016-10-26 17:49:34 -0700

    to file or not file

--- ott/loader/otp/preflight/tests_to_urls.py
+++ ott/loader/otp/preflight/tests_to_urls.py
@@ -3,23 +3,29 @@ import sys
 from ott.utils import otp_utils
 from test_suite import ListTestSuites
 
-def to_urls(name, hostname, port, filter, ws_path):
+def to_urls(name, hostname, port, filter, ws_path, to_file=True, file_name=None):
     #import pdb; pdb.set_trace()
     ws_url, map_url = otp_utils.get_test_urls_from_config(hostname=hostname, port=port, ws_path=ws_path)
     lts = ListTestSuites(ws_url=ws_url, map_url=map_url, filter=filter)
 
-    # make .urls file name
-    flt = "" if filter is None else "-{}".format(filter)
-    file_name = "{}{}.urls".format(name, flt)
+    if file_name or to_file:
+        if file_name is None:
+            # make .urls file name
+            flt = "" if filter is None else "-{}".format(filter)
+            file_name = "{}{}.urls".format(name, flt)
 
-    # write urls to file
-    with open(file_name, 'w') as f:
-        f.write(lts.printer())
+        # write urls to file
+        with open(file_name, 'w') as f:
+            f.write(lts.printer())
+    else:
+        print lts.printer()
 
 def main():
     parser = otp_utils.get_initial_arg_parser()
     parser.add_argument('--hostname', '-hn', '-d', help="specify the hostname for the test url")
     parser.add_argument('--ws_path',  '-ws', help="OTP url path, ala 'prod' or '/otp/routers/default/plan'")
+    parser.add_argument('--print',    '-p',  help="print to stdout rather than a file")
+    parser.add_argument('--filename', '-f',  help="filename")
     args = parser.parse_args()
 
     graphs = otp_utils.get_graphs_from_config()
