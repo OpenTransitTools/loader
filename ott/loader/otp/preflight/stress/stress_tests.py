cfcb36d2b25a2b9b3a91a8de3415311859418c81
Frank Purcell <fxpurcell@gmail.com>
2016-10-31 17:56:16 -0700

    add the stress run routines

--- ott/loader/otp/preflight/stress/stress_tests.py
+++ ott/loader/otp/preflight/stress/stress_tests.py
@@ -19,9 +19,10 @@ class StressTests(CacheBase):
         super(StressTests, self).__init__('otp')
 
         parser = tests_to_urls.get_args_parser()
-        parser.add_argument('--threads',  '-t', default=10, help="number of threads")
-        parser.add_argument('--number',   '-n', type=int, help="number of iterations")
-        parser.add_argument('--duration', '-d', type=int, help="length of time (seconds) to run (as opposed to --number of iterations)")
+        parser.add_argument('--threads',     '-t',  default=10, help="number of threads")
+        parser.add_argument('--number',      '-n',  type=int, help="number of iterations")
+        parser.add_argument('--duration',    '-d',  type=int, help="length of time (seconds) to run (as opposed to --number of iterations)")
+        parser.add_argument('--file_prefix', '-fp', default="stress-", help="stress file prefix, ala : stress-1.txt")
         args = parser.parse_args()
 
         self.args = args
@@ -38,7 +39,7 @@ class StressTests(CacheBase):
         while now < end:
             now = datetime.datetime.now()
             i += 1
-            print i
+            print "{}{}-{}".format(self.args.file_prefix, i, now)
 
     def iterations_stress_test(self, num_iterations):
         for i in range(num_iterations):
