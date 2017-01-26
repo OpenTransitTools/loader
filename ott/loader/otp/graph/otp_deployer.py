5c9eedc6cb68c7ea1696d301bd6f8ff223c2aecc
Frank Purcell <fxpurcell@gmail.com>
2017-01-25 16:32:43 -0800

    scp-ing over to maps8 working well from PC

--- ott/loader/otp/graph/otp_deployer.py
+++ ott/loader/otp/graph/otp_deployer.py
@@ -23,6 +23,11 @@ class OtpDeployer(OtpBuilder):
         ret_val = True
 
         def scp_graph(server, user, graph_dir, server_dir):
+            """ sub-routine to scp Graph.obj-new, otp.v-new and (optionally) otp.jar-new over to
+                a given server.  crazy part of this code is all the path (string) manipulation
+                in step 1 below...
+            """
+            global ret_val
 
             # step 1: create file paths to *-new files locally, and also path where we'll scp these files
             log_v_path = otp_utils.get_vlog_file_path(graph_dir)
@@ -31,34 +36,38 @@ class OtpDeployer(OtpBuilder):
 
             graph_path = otp_utils.get_graph_path(graph_dir)
             graph_new = file_utils.make_new_path(graph_path)
+            graph_svr = file_utils.append_to_path(server_dir, os.path.basename(graph_new), False)
 
             jar_path = otp_utils.get_otp_path(graph_dir)
             jar_new = file_utils.make_new_path(jar_path)
+            jar_svr = file_utils.append_to_path(server_dir, os.path.basename(jar_new), False)
 
-            if file_utils.exists(log_v_new):
-                scp, ssh = web_utils.scp_client(host=server, user=user)
-                scp.put(log_v_new, log_v_svr)
-                return
-
+            # step 2: we are going to attempt to scp Graph.obj-new over to the server(s)
+            #         note: the server paths (e.g., graph_svr, etc...) are relative to the user's home account
             if file_utils.exists(log_v_new) and file_utils.is_min_sized(graph_new):
                 scp = None
                 try:
                     log.info("scp {} over to {}@{}:{}".format(graph_new, user, server, graph_svr))
                     scp, ssh = web_utils.scp_client(host=server, user=user)
-                    scp.put(graph)
-                    scp.put(log_v)
+                    scp.put(graph_new, graph_svr)
+                    scp.put(log_v_new, log_v_svr)
                     if file_utils.is_min_sized(jar_new):
-                        scp.put(jar)
+                        scp.put(jar_new, jar_svr)
                 except Exception, e:
                     log.warn(e)
+                    ret_val = False
                 finally:
                     if scp:
                         scp.close()
 
+        # step A: grab config .ini (from app.ini) variables for the server(s) to scp OTP graphs to
+        #         note, we need these server(s) to be 'known_hosts'
         # import pdb; pdb.set_trace()
         user = self.config.get_json('user', section='deploy')
         servers = self.config.get_json('servers', section='deploy')
         svr_base_dir = self.config.get_json('svr_base_dir', section='deploy')
+
+        # step B: loop thru each server, and scp a graph (and log and jar) to that server
         for s in servers:
             for g in self.graphs:
                 dir = otp_utils.config_graph_dir(g, self.this_module_dir)
