6436ae850219b90e323a7d6f38b226d065375ad7
Frank Purcell <fxpurcell@gmail.com>
2017-03-31 16:52:50 -0700

    csv output added

--- ott/loader/geocoder/db_export/landmarks.py
+++ ott/loader/geocoder/db_export/landmarks.py
@@ -1,10 +1,15 @@
+""" NOTE: below there are queries that are specific to TriMet's geo database (PostGIS), and a landmarks table.
+"""
+import os
+
+from ott.utils import file_utils
 from ott.utils.cache_base import CacheBase
 
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.schema import MetaData
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from geoalchemy2.shape import to_shape
+from geoalchemy2.shape import to_shape  # don't delete me, if you want the geom column to be a WKB type
 
 
 
@@ -31,8 +36,10 @@ class Landmarks(CacheBase):
         #self.schema = self.config.get('transit_schema', section='db', def_val="TRIMET")
         self.db_url = self.config.get('transit_url',    section='db', def_val='postgresql+psycopg2://geoserve@maps6:5432/trimet')
         self.schema = self.config.get('transit_schema', section='db', def_val="current")
+        csv_output = self.config.get('landmarks_csv', section='geocoder', def_val="TRIMET-landmarks.csv")
+        self.file_path = os.path.join(self.cache_dir, csv_output)
 
-    def x(self):
+    def query_and_output(self):
         # engine, suppose it has two tables 'user' and 'address' set up
         engine = create_engine(self.db_url)
 
@@ -45,20 +52,18 @@ class Landmarks(CacheBase):
         LandmarksDao = Base.classes.landmark_ext
         session = Session(engine)
 
+        fp = open(self.file_path, 'w')
+        csv_writer = file_utils.make_csv_writer(fp, ['name', 'address', 'lon', 'lat', 'layer_id'])
+
         type_ids = types.keys()
-        for a in session.query(LandmarksDao).all():
+        for i, a in enumerate(session.query(LandmarksDao).all()):
             if a.type in type_ids:
-                #print a.__dict__
+                row = {'name':a.name, 'address':a.address, 'lon':a.lon, 'lat':a.lat, 'layer_id':types[a.type]}
+                csv_writer.writerow(row)
                 #print to_shape(a.geom).x
                 #print to_shape(a.geom).y
-                print types[a.type]
-                print a.lon
-                print a.lat
-                print a.name
-                print a.address
-                break
 
     @classmethod
     def export(cls):
         l = Landmarks()
-        l.x()
+        l.query_and_output()
