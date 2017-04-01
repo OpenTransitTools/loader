d8e39de2931b4a610d9e3364810c0e453855385a
Frank Purcell <fxpurcell@gmail.com>
2017-03-31 17:20:54 -0700

    refactor with base class

---
+++ ott/loader/geocoder/db_export/db_exporter.py
@@ -0,0 +1,39 @@
+from ott.utils.cache_base import CacheBase
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.schema import MetaData
+from sqlalchemy import create_engine
+from geoalchemy2.shape import to_shape  # don't delete me, if you want the geom column to be a WKB type
+
+import logging
+log = logging.getLogger(__file__)
+
+
+class DbExporter(CacheBase):
+    """ export Transit data from 
+    """
+    def __init__(self):
+        super(DbExporter, self).__init__('geocoder')
+        self.db_url = self.config.get('transit_url',    section='db', def_val='postgresql+psycopg2://geoserve@maps6:5432/trimet')
+        self.schema = self.config.get('transit_schema', section='db', def_val="current")
+
+    def make_database(self):
+        # engine, suppose it has two tables 'user' and 'address' set up
+        self.engine = create_engine(self.db_url)
+
+        # reflect the tables
+        meta = MetaData(schema=self.schema)
+        Base = automap_base(bind=self.engine, metadata=meta)
+        Base.prepare(self.engine, reflect=True)
+        return Base
+
+    def get_table_orm(self, table_name):
+        Base = self.make_database()
+        TableOrm = Base.classes[table_name]
+        #print Base.classes.keys()
+        return TableOrm
+
+    @classmethod
+    def export(cls):
+        log.warn("abstract base method ... no export work being done")
+        pass
--- ott/loader/geocoder/db_export/intersections.py
+++ ott/loader/geocoder/db_export/intersections.py
@@ -1,4 +1,4 @@
-""" NOTE: below there are queries that are specific to TriMet's geo database (PostGIS), and a landmarks table.
+""" NOTE: below there are queries that are specific to TriMet's geo database (PostGIS), and a street intersection table.
 """
 import os
 from ott.utils import file_utils
@@ -9,38 +9,28 @@ import logging
 log = logging.getLogger(__file__)
 
 
-# map TriMet landmark types to something more readable / relatable
-LANDMARK_TYPES = {}
-LANDMARK_TYPES[7] = 'station'
-LANDMARK_TYPES[10] = 'pr'
-LANDMARK_TYPES[14] = 'tc'
-LANDMARK_TYPES[15] = 'facility'
-LANDMARK_TYPES[16] = 'fare_outlet'
-LANDMARK_TYPES[26] = 'tvm'
-
-
-class Landmarks(DbExporter):
-    """ export Transit data from 
+class Intersections(DbExporter):
+    """ export Transit data from
     """
     def __init__(self):
-        super(Landmarks, self).__init__()
-        csv_output = self.config.get('landmarks_csv', section='geocoder', def_val="TRIMET-landmarks.csv")
+        super(Intersections, self).__init__()
+        csv_output = self.config.get('intersections_csv', section='geocoder', def_val="intersections.csv")
         self.file_path = os.path.join(self.cache_dir, csv_output)
 
     def query_and_output(self):
-        LandmarksOrm = self.get_table_orm('landmark_ext')
+        IntersectionsOrm = self.get_table_orm('intersections')
         session = Session(self.engine)
         fp = open(self.file_path, 'w')
         csv_writer = file_utils.make_csv_writer(fp, ['name', 'address', 'lon', 'lat', 'layer_id'])
 
         type_ids = LANDMARK_TYPES.keys()
-        for i, a in enumerate(session.query(LandmarksOrm).all()):
+        for i, a in enumerate(session.query(IntersectionsOrm).all()):
             if a.type in type_ids:
                 row = {'name':a.name, 'address':a.address, 'lon':a.lon, 'lat':a.lat, 'layer_id':LANDMARK_TYPES[a.type]}
                 csv_writer.writerow(row)
 
     @classmethod
     def export(cls):
-        l = Landmarks()
+        l = Intersections()
         l.query_and_output()
 
