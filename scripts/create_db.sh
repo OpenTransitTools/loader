e50d596cc26a44aa00541e70974d11e34c7fe805
Frank Purcell <fxpurcell@gmail.com>
2017-08-03 15:52:24 -0700

    fix create script (no spaces)

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -2,10 +2,10 @@
 ## crete OTT spatial db for OTT
 ##
 
-mac_psql = "/Applications/Postgres.app/Contents/Versions/9.4/bin/psql"
-psql = $mac_psql
-user = ott
-db = ott
+mac_psql=/Applications/Postgres.app/Contents/Versions/9.4/bin/psql
+psql=$mac_psql
+user=ott
+db=ott
 
 $psql -c "CREATE USER ${user};"
 $psql -c "CREATE DATABASE ${db} WITH OWNER ${user};"
