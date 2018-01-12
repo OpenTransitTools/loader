c5c7a117fba2391ebe1bc418cfaedb17d9559e58
Purcell, Frank <purcellf@trimet.org>
2018-01-12 13:35:44 -0800

    fix up the create script

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -3,12 +3,22 @@
 ##
 
 mac_psql=/Applications/Postgres.app/Contents/Versions/9.4/bin/psql
-psql=$mac_psql
+unix_psql=`which psql`
+
+if [ -f "$mac_psql" ]
+then
+    psql=$mac_psql
+elif [ -f "$unix_psql" ]
+then
+    psql=$unix_psql
+fi
+
+def_db=postgres
 user=ott
 db=ott
 
-$psql -c "CREATE USER ${user};"
-$psql -c "CREATE DATABASE ${db} WITH OWNER ${user};"
+$psql -d $def_db -c "CREATE USER ${user};"
+$psql -d $def_db -c "CREATE DATABASE ${db} WITH OWNER ${user};"
 $psql -d $db -c "CREATE EXTENSION postgis;"
 
 
