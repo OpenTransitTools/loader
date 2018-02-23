c058ef492981897a152fee58745d33afaa7d06b3
Frank Purcell <fxpurcell@gmail.com>
2018-02-23 10:52:30 -0800

    add test solr call

--- ott/loader/otp/graph/test/client/js/otp/config.js
+++ ott/loader/otp/graph/test/client/js/otp/config.js
@@ -10,12 +10,13 @@ otp_consts = {
      */
     hostname       : "",
     restService    : "otp_test",
-    solrService    : "http://maps.trimet.org/solr/select",
+    //solrService    : "http://maps.trimet.org/solr/select",
+    solrService    : "http://localhost:45454/solr/select",
     center         : new L.LatLng(45.494833,-122.670376),
     maxWalk        : 804.672, // 1/2 mile walk
     //maxWalk        : 1207.008, // 3/4 mile walk
     //maxWalk        : 1609.344, // 1 mile walk
-    attribution    : 'Map data &copy; 2017 Oregon Metro and <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
+    attribution    : 'Map data &copy; 2018 Oregon Metro and <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.'
 };
 
 otp.config = {
