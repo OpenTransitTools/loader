19193f017cd27600e6493a492cd0804ad9db33a9
Frank Purcell <fxpurcell@gmail.com>
2018-04-30 22:48:53 -0700

    refactor load methods

--- ott/loader/gtfsdb_realtime/gtfsdb_realtime_loader.py
+++ ott/loader/gtfsdb_realtime/gtfsdb_realtime_loader.py
@@ -23,37 +23,10 @@ class GtfsdbRealtimeLoader(CacheBase):
         self.feeds = gtfs_utils.get_realtime_feed_from_config(self.config)
         self.db_url = self.config.get('url', section='db', def_val='postgresql+psycopg2://ott@127.0.0.1:5432/ott')
 
-    def load_feeds_via_config(self, feed, db_url, is_geospatial=True, create_db=False):
-        """
-        insert a GTFS feed into configured db
-        """
-        ret_val = True
-
-        # step 1: agency and schema
-        agency_id = feed.get('agency_id')
-        schema = feed.get('schema', agency_id.lower())
-
-        # step 2: get urls to this feed's
-        trips_url = gtfs_utils.get_realtime_trips_url(feed)
-        alerts_url = gtfs_utils.get_realtime_alerts_url(feed)
-        vehicles_url = gtfs_utils.get_realtime_vehicles_url(feed)
-
-        # step 3: load them there gtfs-rt feeds
-        try:
-            log.info("loading gtfsdb_realtime db {} {}".format(db_url, schema))
-            session = loader.make_session(db_url, schema, is_geospatial, create_db)
-            ret_val = loader.load_agency_feeds(session, agency_id, trips_url, alerts_url, vehicles_url)
-        except Exception as e:
-            log.error("DATABASE ERROR : {}".format(e))
-            ret_val = False
-
-        return ret_val
-
     def load_all(self, app_id=None, is_geospatial=True, create_db=False):
         for f in self.feeds:
-            if app_id:
-                f.app_id = app_id
-            self.load_feeds_via_config(f, self.db_url, is_geospatial, create_db)
+            if app_id: f.app_id = app_id
+            loader.load_feeds_via_config(f, self.db_url, is_geospatial, create_db)
 
     @classmethod
     def load(cls):
