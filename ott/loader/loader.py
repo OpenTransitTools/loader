c5ee2762eb8ade725c1f827e87b55c738bceb980
Frank Purcell <fxpurcell@gmail.com>
2018-05-02 21:08:17 -0700

    add gtfsdb_realtime on load ... creates db

--- ott/loader/loader.py
+++ ott/loader/loader.py
@@ -5,6 +5,7 @@ from ott.loader.gtfs.gtfs_cache import GtfsCache
 from ott.loader.otp.graph.otp_exporter import OtpExporter
 from ott.loader.otp.graph.otp_builder import OtpBuilder
 from ott.loader.gtfsdb.gtfsdb_loader import GtfsdbLoader
+from ott.loader.gtfsdb_realtime.gtfsdb_realtime_loader import GtfsdbRealtimeLoader
 from ott.loader.sum.sum_cache import SumCache
 from ott.loader.solr.solr_loader import SolrLoader
 
@@ -60,11 +61,15 @@ def load_all():
     db = GtfsdbLoader()
     db.check_db(force_update=force_update)
 
-    log.info("step 5: load otp (build new graph)")
+    log.info("step 5: load gtfsdb_realtime db")
+    rt = GtfsdbRealtimeLoader()
+    rt.load_all(is_geospatial=True, create_db=True)
+
+    log.info("step 6: load otp (build new graph)")
     otp = OtpBuilder(force_update=force_update)
     otp.build_and_test_graphs(force_update=force_update)
 
-    log.info("step 6: load various data layers into SOLR")
+    log.info("step 7: load various data layers into SOLR")
     solr_load = SolrLoader.load()
 
 
