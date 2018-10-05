fb201ca21e6dd86e8c4196ca259307bb01b22cbc
Frank Purcell <fxpurcell@gmail.com>
2018-10-04 23:12:54 -0700

    refactor the build stuff, so that rebuilt graphs w/skip tests (e.g MOD and test) will get an otp.v and -new treatment for deployment

--- ott/loader/otp/graph/otp_builder.py
+++ ott/loader/otp/graph/otp_builder.py
@@ -58,35 +58,13 @@ class OtpBuilder(CacheBase):
                     GtfsCache.check_feeds_against_cache(self.feeds, graph_dir, force_update, filter)
         return graphs
 
-    def build_and_test_graphs(self, java_mem=None, force_update=False, start_server=True):
-        """ will build and test each of the graphs we have in self.graphs
+    def update_vlog(self, graph):
+        """ print out gtfs feed(s) version numbers and dates to the otp.v log file
         """
-        ret_val = True
-        if self.graphs:
-            for g in self.graphs:
-                success, rebuilt = self.build_graph(g['dir'], java_mem, force_update)
-                if success and rebuilt and not g.get('skip_tests'):
-                    success = self.test_graph(graph=g, java_mem=java_mem, start_server=start_server)
-                    ret_val = success
-                elif not success:
-                    ret_val = False
-                    log.warn("graph build failed for graph {}".format(g['name']))
-                if rebuilt and g.get('post_shutdown'):
-                    otp_utils.kill_otp_server(g['dir'])
-        return ret_val
-
-    def only_test_graphs(self, java_mem=None, break_on_fail=False, start_server=True):
-        """ will test each of the graphs we have in self.graphs
-        """
-        ret_val = True
-        if self.graphs:
-            for g in self.graphs:
-                success = self.test_graph(graph=g, java_mem=java_mem, start_server=start_server)
-                if not success:
-                    ret_val = False
-                    if break_on_fail:
-                        break
-        return ret_val
+        graph_dir = graph.get('dir', self.cache_dir)
+        gtfs_msg = GtfsInfo.get_cache_msgs(graph_dir, self.feeds, graph.get('filter'))
+        osm_msg = OsmInfo.get_cache_msgs(graph_dir)
+        otp_utils.append_vlog_file(graph_dir, gtfs_msg + osm_msg)
 
     def build_graph(self, graph_dir, java_mem=None, force_update=False):
         """ will rebuild the graph...
@@ -108,8 +86,9 @@ class OtpBuilder(CacheBase):
         # step 4: build graph is needed
         if rebuild_graph:
             success = False
-            # step 5a: run the builder multiple times until we get a good looking Graph.obj
-            for n in range(1, 21):
+
+            # step 4b: run the builder multiple times until we get a good looking Graph.obj
+            for n in range(1, 5):
                 log.info(" build attempt {0} of a new graph ".format(n))
                 otp_utils.run_graph_builder(graph_dir, java_mem=java_mem)
                 time.sleep(60)
@@ -119,7 +98,9 @@ class OtpBuilder(CacheBase):
                 else:
                     log.warn("\n\nGRAPH DIDN'T BUILD ... WILL TRY TO BUILD AGAIN\n\n")
                     time.sleep(3)
+
         return success, rebuild_graph
+
 
     def test_graph(self, graph, suite_dir=None, java_mem=None, start_server=True):
         """ will test a given graph against a suite of tests
@@ -131,22 +112,103 @@ class OtpBuilder(CacheBase):
             delay = 60
         if success:
             success = TestRunner.test_graph_factory_config(graph, suite_dir=suite_dir, delay=delay)
-            if success:
-                self.update_vlog(graph=graph)
-                otp_utils.package_new(graph_dir=graph['dir'], graph_name=self.graph_name)
-            else:
+            if not success:
                 log.warn("graph {} didn't pass it's tests".format(graph['name']))
         else:
             log.warn("was unable to run OTP server for graph {}".format(graph['name']))
         return success
 
-    def update_vlog(self, graph):
-        """ print out gtfs feed(s) version numbers and dates to the otp.v log file
+
+    def build_and_test_graphs(self, java_mem=None, force_update=False, start_server=True, graph_filter=None):
+        """ will build and test each of the graphs we have in self.graphs
         """
-        graph_dir = graph.get('dir', self.cache_dir)
-        gtfs_msg = GtfsInfo.get_cache_msgs(graph_dir, self.feeds, graph.get('filter'))
-        osm_msg = OsmInfo.get_cache_msgs(graph_dir)
-        otp_utils.append_vlog_file(graph_dir, gtfs_msg + osm_msg)
+        ret_val = True
+        if self.graphs:
+
+            # step 1: loop thru all graph configs ... building and testing new graphs
+            for g in self.graphs:
+
+                # step 1b: if we're filtering graphs by name, only run that specific graph
+                if graph_filter and g['name'] != graph_filter: continue
+
+                # step 2: build this graph
+                success, rebuilt = self.build_graph(g['dir'], java_mem, force_update)
+
+                # step 3: test the successfully built new graph (restarting a new OTP server for the graph)
+                if success and rebuilt and not g.get('skip_tests'):
+                    success = self.test_graph(graph=g, java_mem=java_mem, start_server=start_server)
+                    ret_val = success
+
+                # step 3b: failed to build the graph ... send a warning
+                elif not success:
+                    ret_val = False
+                    log.warn("graph build failed for graph {}".format(g['name']))
+
+                # step 4: so we rebuilt the graph and any testing that was done was also a success...
+                if rebuilt:
+
+                    # step 4b: update the vlog and package the graph as new
+                    if success:
+                        self.update_vlog(graph=g)
+                        otp_utils.package_new(graph_dir=g['dir'], graph_name=self.graph_name)
+
+                    # step 4c: shut down any graph that
+                    if g.get('post_shutdown'):
+                        otp_utils.kill_otp_server(g['dir'])
+        return ret_val
+
+    def only_test_graphs(self, java_mem=None, break_on_fail=False, start_server=True, graph_filter=None):
+        """ will test each of the graphs we have in self.graphs
+        """
+        ret_val = True
+        if self.graphs:
+            for g in self.graphs:
+                if graph_filter and g['name'] != graph_filter: continue
+                success = self.test_graph(graph=g, java_mem=java_mem, start_server=start_server)
+                if not success:
+                    ret_val = False
+                    if break_on_fail:
+                        break
+        return ret_val
+
+    @classmethod
+    def build(cls):
+        """ effectively the main routine for building new graphs from the command line """
+        success = False
+
+        # step 1: config the builder system
+        args, parser = OtpBuilder.get_args()
+        b = OtpBuilder(force_update=args.force, dont_update=args.dont_update)
+        java_mem = "-Xmx1236m" if args.mem else None
+
+        if args.mock:
+            # step 2: just going to much with the vlogs, etc... as a mock build & test
+            graph = otp_utils.find_graph(b.graphs, args.name)
+            b.update_vlog(graph)
+            success = True
+        else:
+            # step 3: we're going to try and build 1 or all graphs in the config
+
+            # step 3a: are we being asked to filter only one graph to build and/or test ?
+            graph_filter = None
+            if args.name != "all":
+                graph = otp_utils.find_graph(b.graphs, args.name)
+                if graph:
+                    graph_filter = args.name
+                else:
+                    graph_filter = "unknown graph"
+                    log.warn("I don't know how to build graph '{}'".format(args.name))
+
+            # step 3b: build and/or test one or all graphs in the config file (won't do anything with an "unknown" graph name
+            if args.test:
+                success = b.only_test_graphs(java_mem=java_mem, start_server=not args.dont_restart, graph_filter=graph_filter)
+            else:
+                success = b.build_and_test_graphs(java_mem=java_mem, force_update=args.force, start_server=not args.dont_restart, graph_filter=graph_filter)
+
+        if args.email and (not success or args.force):
+            otp_utils.send_build_test_email(args.email)
+
+        return success
 
     @classmethod
     def get_args(cls):
@@ -164,49 +226,6 @@ class OtpBuilder(CacheBase):
 
         args = parser.parse_args()
         return args, parser
-
-    @classmethod
-    def build(cls):
-        """ effectively the main routine for building new graphs from the command line """
-        success = False
-
-        build_success = True
-        test_success = True
-        server_success = True
-
-        args, parser = OtpBuilder.get_args()
-        b = OtpBuilder(force_update=args.force, dont_update=args.dont_update)
-        java_mem = "-Xmx1236m" if args.mem else None
-
-        if args.mock:
-            graph = otp_utils.find_graph(b.graphs, args.name)
-            b.update_vlog(graph)
-            # TODO: implement mv graph to good
-            # b.mv_failed_graph_to_good()
-            success = True
-        else:
-            if args.name != "all":
-                graph = otp_utils.find_graph(b.graphs, args.name)
-                if graph:
-                    # either build and/or test a single named graph
-                    if not args.test:
-                        success, rebuilt = b.build_graph(graph['dir'], java_mem=java_mem, force_update=args.force)
-                    if not args.no_tests:
-                        success = b.test_graph(graph, java_mem=java_mem, start_server=not args.dont_restart)
-                else:
-                    log.warn("I don't know how to build graph '{}'".format(args.name))
-                    success = False
-            else:
-                # build and/or test all graphs in the config file
-                if args.test:
-                    success = b.only_test_graphs(java_mem=java_mem, start_server=not args.dont_restart)
-                else:
-                    success = b.build_and_test_graphs(java_mem=java_mem, force_update=args.force, start_server=not args.dont_restart)
-
-        if args.email and (not success or args.force):
-            otp_utils.send_build_test_email(args.email)
-
-        return success
 
 
 def main(argv=sys.argv):
