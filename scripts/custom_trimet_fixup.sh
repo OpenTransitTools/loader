c589b32f48fb9d751489bfa11450a1d02b143581
Frank Purcell <fxpurcell@gmail.com>
2019-02-01 16:37:39 -0800

    change the fixup to use PSC and also not MAX and PSC

--- scripts/custom_trimet_fixup.sh
+++ scripts/custom_trimet_fixup.sh
@@ -1,1 +1,2 @@
-psql -d ott -c "update trimet.current_stops set route_type=111, route_mode='SC' where route_short_names like '%Streetcar%'"
+psql -d ott -c "update trimet.current_stops set route_type=111, route_mode='SC' where route_short_names like '%PSC%' and route_short_names not like '%MAX%'"
+
