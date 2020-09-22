712fecbf5e8cdfb5a46eefaff35d2d75da3eeafe
Frank Purcell <fxpurcell@gmail.com>
2020-09-21 21:37:58 -0700

    fix(config): clean up deps for OSM

--- ott/loader/osm/osmosis/install.sh
+++ ott/loader/osm/osmosis/install.sh
@@ -1,13 +1,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
-FILE="osmosis-latest.tgz"
-
 cd $DIR
 echo "I'm in $PWD"
 
-cp ~/cache/osmosis*.tgz $FILE
+# OSMOSIS download as of (Sept) 2020:
+#   https://github.com/openstreetmap/osmosis/releases/download/0.48.3/osmosis-0.48.3.tgz
+URL=https://github.com/openstreetmap/osmosis/releases/download
+VER=0.48.3
+EXT=tgz
+FILE=osmosis-$VER.$EXT
+
 if [ ! -f $FILE ];
 then
-    curl https://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.tgz > $FILE
+    echo "PLEAZZE CHEEEECK LATEST VERSION (currently using version $VER):"
+    echo "https://github.com/openstreetmap/osmosis/releases/latest/"
+    echo
+    curl -L $URL/$VER/$FILE > $FILE
 fi
 tar xvfz $FILE
 chmod a+x bin/osmosis
