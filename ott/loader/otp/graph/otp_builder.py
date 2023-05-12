52669fbedbe0bb42cb37d1aad77f5eae81341fb8
Frank Purcell <fxpurcell@gmail.com>
2023-05-11 21:25:18 -0700

    fix: some defaults

--- ott/loader/otp/graph/otp_builder.py
+++ ott/loader/otp/graph/otp_builder.py
@@ -157,11 +157,10 @@ class OtpBuilder(CacheBase):
 
                 # step 4: so we rebuilt the graph and any testing that was done was also a success...
                 if rebuilt:
-
                     # step 4b: update the vlog and package the graph as new
                     if success:
                         self.update_vlog(graph=g)
-                        otp_utils.package_new(graph_dir=g.get('dir', './'), graph_name=g.get('graph_name', otp_utils.DEF_GRAPH_NAME))
+                        otp_utils.package_new(graph_dir=g.get('dir', './'), graph_version=g.get('graph_version', otp_utils.OTP_VERSION))
 
                     # step 4c: shut down any graph that
                     if g.get('post_shutdown'):
