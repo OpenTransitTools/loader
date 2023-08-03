09e2369a02dd105937782ad3fef7f4aaba73f237
Frank Purcell <fxpurcell@gmail.com>
2023-08-03 13:37:10 -0700

    feat: add url to create script

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -13,14 +13,20 @@ then
     psql=$unix_psql
 fi
 
+db_url=$1
 def_db=postgres
 user=ott
 db=ott
 osm_db=osm
 
+# use URL if we get content on the cmd line (default to docker url when no ://)
+if  [[ "$db_url" != "" ]] && [[ "$db_url" != *"://"* ]]; then
+    db_url=postgres://docker:docker@localhost:5432/
+fi
+
 for d in $db $osm_db
 do
-  $psql -d $def_db -c "CREATE USER ${user};"
-  $psql -d $def_db -c "CREATE DATABASE ${d} WITH OWNER ${user};"
-  $psql -d ${d} -c "CREATE EXTENSION postgis;"
+  $psql -d ${db_url}${def_db} -c "CREATE USER ${user};"
+  $psql -d ${db_url}${def_db} -c "CREATE DATABASE ${d} WITH OWNER ${user};"
+  $psql -d ${db_url}${d} -c "CREATE EXTENSION postgis;"
 done
