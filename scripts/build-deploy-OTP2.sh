173381938755d23890f7ff1dd3915986373674b7
Frank Purcell <fxpurcell@gmail.com>
2023-10-06 11:26:19 -0700

    feat: allow custom GTFS (Fares v2) to get built via the OTP2 script

--- scripts/build-deploy-OTP2.sh
+++ scripts/build-deploy-OTP2.sh
@@ -1,4 +1,11 @@
-OTP2
-bin/otp_build call-test --force
+. ~/secrets
+export JAVA_HOME=$HOME/jdk_17
+export PATH="$JAVA_HOME/bin:.:/home/otp/install/mvn/bin:$PATH"
+export JAVA_OPTS="-Xms2298m -Xmx4096m -server"
+
+rm ott/loader/otp/graph/call-test/graph.obj
+bin/otp_build -d call-test
 bin/otp_package_new call-test
 bin/otp_export -svr maps8
+
+touch ~/l*/o*/l*/o*/g*/*/*obj*
