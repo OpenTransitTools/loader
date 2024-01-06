3e9b594cc77dbc1309483a389b527e89747930d7
Frank Purcell <fxpurcell@gmail.com>
2024-01-05 17:23:47 -0800

    fix: scp data to dev server

--- scripts/build-deploy-OTP2.sh
+++ scripts/build-deploy-OTP2.sh
@@ -1,10 +1,13 @@
-. ~/secrets
 export JAVA_HOME=$HOME/jdk_21
 export PATH="$JAVA_HOME/bin:.:/home/otp/install/mvn/bin:$PATH"
 export JAVA_OPTS="-Xms2298m -Xmx4096m -server"
 
+. ~/secrets
+
+
 rm ott/loader/otp/graph/call-test/graph.obj
 bin/otp_build -d call-test
+bin/otp_v_new call-test
 bin/otp_package_new call-test
 bin/otp_export
 
--- scripts/cp_graph_data.sh
+++ scripts/cp_graph_data.sh
@@ -1,7 +1,9 @@
 #
 # COPY a graph directory over to another server
 #
-TO=${1:-'mapdata@cs-pd-mapetl01:~/graph/'}
+
+#TO=${1:-'mapdata@cs-pd-mapetl01:~/graph/'}
+TO=${1:-'otp@rj-dv-mapapp01:~/loader/ott/loader/otp/graph/rtp/'}
 FM=${2:-'./ott/loader/otp/graph/call-test'}
 CP=${3:-'scp'}
 
