758f0bc7890689a38bd01d0956ac809e4e6e1984
Frank Purcell <fxpurcell@gmail.com>
2024-01-17 16:37:05 -0800

    fix: OTP2 builder script with check on MOD graph dates

--- scripts/build-deploy-OTP2.sh
+++ scripts/build-deploy-OTP2.sh
@@ -4,11 +4,24 @@ export JAVA_OPTS="-Xms2298m -Xmx4096m -server"
 
 . ~/secrets
 
+GRAPH=~/loader/ott/loader/otp/graph
+CT=${1:-"$GRAPH/call-test/graph.obj"}
+MOD=${2:-"$GRAPH/mod/Graph.obj"}
+CT_TIME=`stat -c %Y $CT`
+MOD_TIME=`stat -c %Y $MOD`
+if [ $CT_TIME -gt $MOD_TIME ]; then
+  echo "NOT building $CT ($CT_TIME), as it's newer than $MOD ($MOD_TIME)"
+  exit 1
+else
+  echo "BUILDING $CT ($CT_TIME), as it's older than $MOD ($MOD_TIME)"
+fi
 
-rm ott/loader/otp/graph/call-test/graph.obj
+rm $CT
 bin/otp_build -d call-test
 bin/otp_v_new call-test
 bin/otp_package_new call-test
 bin/otp_export
 
-touch ~/l*/o*/l*/o*/g*/*/*obj*
+touch $GRAPH/*/*obj*
+sleep 5
+touch $CT
