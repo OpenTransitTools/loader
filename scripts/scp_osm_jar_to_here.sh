a65608a9e1263ef4b04020ec7011c595953ff81b
Frank Purcell <fxpurcell@gmail.com>
2024-01-26 16:48:46 -0800

    feat: add scp script

---
+++ scripts/scp_osm_jar_to_here.sh
@@ -0,0 +1,17 @@
+fdir=${1-"call-test"}
+tdir=${1-"rtp"}
+svr=${2-"otp@maps6"}
+
+# scp OSM
+osm="loader/ott/loader/osm/cache"
+rm -f ~/rtp/$osm
+mkdir ~/rtp/$osm
+cmd="scp $svr:~/$osm/*[fls] ~/rtp/$osm"
+echo $cmd
+eval $cmd
+
+# scp OSM
+otp="loader/ott/loader/otp/graph"
+cmd="scp $svr:~/$otp/$fdir/*.jar ~/rtp/$otp/$tdir/"
+echo $cmd
+eval $cmd
